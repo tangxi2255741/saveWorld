292555af943f77101514041080bf2fd373a84581
bjtangx <cdtangxi@jd.com>
2018-01-13 22:21:42 +0800

    web项目搭建

--- src/main/java/com/tx/saveWorld/SaveWorldApplication.java
+++ src/main/java/com/tx/saveWorld/SaveWorldApplication.java
@@ -2,19 +2,37 @@ package com.tx.saveWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 项目的启动入口
  */
-@RestController
+@Controller
 @SpringBootApplication
 public class SaveWorldApplication {
 
 	@RequestMapping("/")
-	String index(){
-		return "save world";
+	public String index(Model model){
+		Person single = new Person("aa",11);
+
+		List<Person> people = new ArrayList<Person>();
+		Person p1 = new Person("xx",11);
+		Person p2 = new Person("yy",22);
+		Person p3 = new Person("zz",33);
+		people.add(p1);
+		people.add(p2);
+		people.add(p3);
+
+		model.addAttribute("singlePerson", single);
+		model.addAttribute("people", people);
+
+		return "index";
 	}
 
 	public static void main(String[] args) {
